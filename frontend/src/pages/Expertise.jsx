50c6598f24eae0a122edcf70bba89c9a035926a9
404AmanFound <sant.sharmawelding@gmail.com>
2026-05-17 14:33:18 +0530

    basic setup done

---
+++ frontend/src/pages/Expertise.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Expertise() {
+  return (
+    <div>
+      <h1>Expertise</h1>
+    </div>
+  )
+}
+
+export default Expertise
---
+++ frontend/src/pages/Works.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Works() {
+  return (
+    <div>
+      <h1>
+        Works
+      </h1>
+    </div>
+  )
+}
+
+export default Works
